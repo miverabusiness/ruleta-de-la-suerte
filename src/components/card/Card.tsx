fe326332428f315ab3c595863de7be476b371fc3
miverabusiness <miverabusiness@gmail.com>
2023-10-07 01:44:18 +0200

    Panel v0.2

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
 
-function Card() {
+function Card(props: { letra: string }) {
+	const [mostrarLetra, setMostrarLetra] = useState(false);
+
+	const handleClick = () => {
+		setMostrarLetra(!mostrarLetra);
+	};
+
+	const isLetterSpace = () => {
+		return props.letra === " " ? " space" : mostrarLetra ? " correct" : " not-yet";
+	};
+
 	return (
 		<>
-			<button className="letter-card">Card</button>
+			<button className={"letter-card" + isLetterSpace()} onClick={handleClick}>
+				{mostrarLetra ? props.letra : ""}
+			</button>
 		</>
 	);
 }
--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -2,71 +2,47 @@ import React from "react";
 import "./Panel.css";
 import Card from "../card/Card";
 
+const N_FILAS = 4;
+const FILA_CORTA = 12;
+const FILA_LARGA = 14;
+
+let panel = [];
+
+for (let i = 0; i < N_FILAS; i++) {
+	if (i === 0 || i === N_FILAS - 1) {
+		panel.push(Array(FILA_CORTA).fill("A"));
+	} else {
+		panel.push(Array(FILA_LARGA).fill("E"));
+	}
+}
+
+panel = [
+	[" ", "A", "L", "A", "I", "N", "B", "O", "S", "S", " ", " "], //12
+	["P", "E", "D", "A", "Z", "O", " ", "D", "E", " ", "R", "A", "T", "A"], //14
+	["M", "A", "R", "I", "C", "O", "N", " ", "T", "E", " ", "V", "O", "Y"], //14
+	["A", " ", "F", "O", "I", "A", "R", " ", "R", "I", "C", "O"], //12
+];
+
 function Panel() {
-	return (
-		<>
-			<div className="tablero">
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+	const filas = [];
+	for (let i = 0; i < N_FILAS; i++) {
+		const cards = [];
+		const longitudFila = i === 0 || i === N_FILAS - 1 ? FILA_CORTA : FILA_LARGA;
+
+		for (let j = 0; j < longitudFila; j++) {
+			cards.push(<Card key={j} letra={panel[i][j]} />);
+		}
+
+		filas.push(
+			<div className="fila" key={i}>
+				{cards}
 			</div>
-			<div className="tablero">
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-			</div>
-			<div className="tablero">
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-			</div>
-			<div className="tablero">
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-			</div>
-		</>
-	);
+		);
+
+		console.error("panel", panel);
+	}
+
+	return <>{filas}</>;
 }
 
 export default Panel;
