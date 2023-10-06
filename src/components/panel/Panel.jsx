d4fd72022a08415959aced8e8b8e485869be7a7a
miverabusiness <miverabusiness@gmail.com>
2023-10-07 00:27:28 +0200

    panel v0

---
+++ src/components/panel/Panel.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Panel.css";
+import Card from "../card/Card";
+
+function Panel() {
+	return (
+		<>
+			<div className="tablero">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+			<div className="tablero">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+			<div className="tablero">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+			<div className="tablero">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</>
+	);
+}
+
+export default Panel;
